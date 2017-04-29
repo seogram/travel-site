30d3c4e0b021b5db0b705e36346b4042e8e5a5bc
Afshin <a.hamedbarghi@gmail.com>
2017-04-29 15:54:06 +0200

    completed the reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,2 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/mobileMenu.js';
+import RevealOnScroll from './modules/revealOnScroll';
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll($(".feature-item"),"85%");
+var revealOnScroll = new RevealOnScroll($(".testimonial"),"65%");
+var revealOnScroll = new RevealOnScroll($(".generic-content-container"),"65%");
